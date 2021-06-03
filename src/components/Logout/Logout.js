10219e7c8cdb6437669191c56b8e355a748b8e42
alyssa reyes <alyssa.va.reyes@gmail.com>
2021-06-03 15:48:53 -0400

    building out client side

---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Logout.css';
+import logoutPic from '../../assets/logoutPic.png';
+
+
+
+
+const Logout = (props) => {
+    return (
+        <div>
+            <img id="logout" src={logoutPic} alt="powerButton" />
+        </div>
+    )
+}
+
+export default Logout;
